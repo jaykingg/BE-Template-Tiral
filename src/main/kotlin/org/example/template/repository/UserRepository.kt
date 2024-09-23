8d5f35c6b8eb7184a71b1512a1e4f776ad01ff8a
jayking <mojunseo49@naver.com>
2024-09-23 15:04:20 +0900

    feat(User): remove import

--- src/main/kotlin/org/example/template/repository/UserRepository.kt
+++ src/main/kotlin/org/example/template/repository/UserRepository.kt
@@ -3,6 +3,5 @@ package org.example.template.repository
 import org.bson.types.ObjectId
 import org.example.template.domain.User
 import org.springframework.data.repository.kotlin.CoroutineCrudRepository
-import org.springframework.data.repository.reactive.ReactiveCrudRepository
 
 interface UserRepository : CoroutineCrudRepository<User, ObjectId>
