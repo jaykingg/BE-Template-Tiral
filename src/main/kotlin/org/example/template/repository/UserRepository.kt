499a958e0de1ec25db68f13041f986dfd0cc708b
jayking <mojunseo49@naver.com>
2024-10-08 13:29:38 +0900

    feat: Response view

--- src/main/kotlin/org/example/template/repository/UserRepository.kt
+++ src/main/kotlin/org/example/template/repository/UserRepository.kt
@@ -1,7 +1,13 @@
 package org.example.template.repository
 
+import kotlinx.coroutines.flow.Flow
 import org.bson.types.ObjectId
 import org.example.template.domain.User
+import org.springframework.data.domain.Pageable
+import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories
 import org.springframework.data.repository.kotlin.CoroutineCrudRepository
 
-interface UserRepository : CoroutineCrudRepository<User, ObjectId>
+@EnableReactiveMongoRepositories
+interface UserRepository : CoroutineCrudRepository<User, ObjectId> {
+    suspend fun findAllBy(pageable: Pageable): Flow<User>
+}
